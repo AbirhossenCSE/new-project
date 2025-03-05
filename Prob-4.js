7cd101928ce75282113569069f00260f1eddf613
Abir Hossen <abir.cse4@gmail.com>
2025-03-05 16:03:56 +0600

    new problem

--- Prob-4.js
+++ Prob-4.js
@@ -113,5 +113,10 @@
 // }
 // foo();
 
+// async function fetchData() {
+//     const response = await fetch("https://api.example.com/data");
+//     const data = await response.json();
+//     return data;
+// }
 
 
